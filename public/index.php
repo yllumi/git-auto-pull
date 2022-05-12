a8a117888e3f2b92d5c93628b933d0ce0f75b85b
Toni Haryanto <toha.samba@gmail.com>
2022-05-12 17:39:11 +0700

    Check if branch pushed suitable with repo being pulled

--- public/index.php
+++ public/index.php
@@ -7,14 +7,25 @@
  */
 $name = $_GET['name'] ?? 'default';
 
+// Get repository list
 $repos = explode("\n", file_get_contents('repos'));
 $repoArray = [];
-foreach ($repos as &$repo) {
- 	$repo = substr($repo, 0, strpos($repo, " "));
-} 
-if(!in_array($name, $repos)) die("Repo name not registered.");
+foreach ($repos as $repo) {
+    $temp = explode(" ", $repo);
+    $repoArray[$temp[0]] = $temp[2]; // name => branch
+}
+
+// Get github payload
+$payload = json_decode(file_get_contents('php://input'), true);
+$ref = explode('/', $payload['ref']);
+$branchPushed = $ref[2];
+
+if(!isset($repoArray[$name])) die("Repo name not registered.");
+if($repoArray[$name] != $branchPushed) die("Pushed branch is not the target for pulling.");
 
 $markFile = fopen("../mark/".$name, "w") or die("Unable to open file.");
 fwrite($markFile, $repos[$name]);
 fclose($markFile);
 echo "Done!";
+
+exec('../pull.sh');
