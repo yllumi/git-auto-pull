be88a36ca4787d77c53e826a123dc75284ea4505
Toni Haryanto <toha.samba@gmail.com>
2022-05-12 19:16:30 +0700

    Add signature checking

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
  */
 $name = $_GET['name'] ?? 'default';
 
+
 // Get repository list
 $repos = explode("\n", file_get_contents('repos'));
 $repoArray = [];
@@ -16,9 +17,17 @@ foreach ($repos as $repo) {
 }
 
 // Get github payload
-$payload = json_decode(file_get_contents('php://input'), true);
-$ref = explode('/', $payload['ref']);
+$payload = file_get_contents('php://input');
+$payloadArr = json_decode($payload, true);
+$ref = explode('/', $payloadArr['ref']);
 $branchPushed = $ref[2];
+
+// Check requirement
+if(file_exists('../secret')){
+    $secret = file_get_contents('../secret');
+    $hash = 'sha256=' . hash_hmac('sha256', $payload, $secret);
+    if(! hash_equals($hash, $_SERVER['HTTP_X_HUB_SIGNATURE_256'])) die("Signature didn't match.");
+}
 
 if(!isset($repoArray[$name])) die("Repo name not registered.");
 if($repoArray[$name] != $branchPushed) die("Pushed branch is not the target for pulling.");
